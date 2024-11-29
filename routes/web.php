a545b951473ca4d483938b5e72e2d584c048342c
Firey-event559 <RaminoHiromiVrca@gmail.com>
2024-11-29 13:15:40 +0100

    Update login

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\auth\LoginController;
 
 Route::get('/', function () {
     return view('index');
 });
 
 Route::get('/admin', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+Route::get('admin/admin-dashboard', function () {
+    return view('admin.admin-dashboard');
+});
+
+Route::get('/admin/admin-dashboard', [LoginController::class, 'dashboard'])->middleware('auth');
+
+Auth::routes();
+
+
