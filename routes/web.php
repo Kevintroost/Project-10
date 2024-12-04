c4cc90c5650461f411b72c15a62906a4f2174d0c
Firey-event559 <RaminoHiromiVrca@gmail.com>
2024-12-04 10:53:47 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,4 @@
+
 <?php
 
 use Illuminate\Support\Facades\Route;
@@ -14,15 +15,22 @@ Route::get('/index', function () {
     return view('index');
 });
 
+Route::get('/admin', function () {
+    return view('auth.login');
+});
 
 
 
 
+Route::group(['middleware' => 'auth'], function () {
     Route::get('/admin/dashboard', function () {
         return view('admin.admin-dashboard');
-        
+
     });
 
+    Route::post('Destroy', [LoginController::class, 'Destroy'])->middleware('auth')->name('logout');
+
+});
 
 
 
@@ -31,5 +39,5 @@ Route::get('/index', function () {
 Auth::routes();
 
 // Event Requests 
-Route::get('event-request/create', [EventRequestController::class, 'create'])->name('event-request.create');
-Route::post('event-request/store', [EventRequestController::class, 'store'])->name('event-request.store');
+Route::get('event-requests/create', [EventRequestController::class, 'create'])->name('event-requests.create');
+Route::post('event-requests/store', [EventRequestController::class, 'store'])->name('event-requests.store');
