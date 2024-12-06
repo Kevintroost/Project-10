00fe85fc7f7d4fda29153b7aa4c9ca6233827f81
KEVN <troostkevin8@gmail.com>
2024-12-06 11:16:35 +0100

    event page

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -1,107 +1,159 @@
 <x-layout>
     <section class="bg-white  antialiased">
-        <div class="max-w-screen-xl px-4 py-8 mx-auto lg:px-6 sm:py-16 lg:py-24">
-            <div class="max-w-3xl mx-auto text-center">
-                <h2 class="text-4xl font-extrabold leading-tight tracking-tight text-gray-900 ">
-                    Events
-                </h2>
-            </div>
+        <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
 
-            <div class="flow-root max-w-3xl mx-auto mt-8 sm:mt-12 lg:mt-16">
-                <div class="-my-4 divide-y divide-gray-200 dark:divide-gray-700">
-                    <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
-                        <p class="w-32 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
-                            08:00 - 09:00
-                        </p>
-                        <h3 class="text-lg font-semibold ">
-                            <a href="#" class="hover:underline">Opening remarks</a>
-                        </h3>
+            <div class="gap-8 items-center py-8 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 ">
+
+                <div class="mt-4 md:mt-0">
+
+                    <div class="max-w-3xl mx-auto text-center">
+                        <h2 class="text-4xl font-extrabold leading-tight tracking-tight text-gray-900 ">
+                            Upcoming Events
+                        </h2>
                     </div>
 
-                    <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
-                        <p class="w-32 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
-                            09:00 - 10:00
-                        </p>
-                        <h3 class="text-lg font-semibold text-gray-900 dark:">
-                            <a href="#" class="hover:underline">Bergside LLC: Controlling the video traffic flows</a>
-                        </h3>
+                    <div class="flow-root max-w-3xl mx-auto mt-8 sm:mt-12 lg:mt-16">
+                        <div class="-my-4 divide-y divide-gray-200 dark:divide-gray-700">
+                            <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
+                                <p
+                                    class="w-40 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
+                                    5 December 20:00
+                                </p>
+                                <h3 class="text-lg font-semibold ">
+                                    <a href="#" class="hover:underline">Slipknot</a>
+                                </h3>
+                            </div>
+
+                            <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
+                                <p
+                                    class="w-40 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
+                                    11 june 20:00
+                                </p>
+                                <h3 class="text-lg font-semibold text-gray-900 dark:">
+                                    <a href="#" class="hover:underline">linkin park</a>
+                                </h3>
+                            </div>
+
+                            <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
+                                <p
+                                    class="w-40 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
+                                    30 august 19:00
+                                </p>
+                                <h3 class="text-lg font-semibold text-gray-900 dark:">
+                                    <a href="#" class="hover:underline">Babymetal</a>
+                                </h3>
+                            </div>
+
+                            <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
+                                <p
+                                    class="w-40 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
+                                    15 february 15:00
+                                </p>
+                                <h3 class="text-lg font-semibold text-gray-900 ">
+                                    <a href="#" class="hover:underline">Pink pop</a>
+                                </h3>
+                            </div>
+
+                            <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
+                                <p
+                                    class="w-40 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
+                                    4 may 13:00
+                                </p>
+                                <h3 class="text-lg font-semibold text-gray-900 ">
+                                    <a href="#" class="hover:underline">Rotterdam rave</a>
+                                </h3>
+                            </div>
+
+                            <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
+                                <p
+                                    class="w-40 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
+                                    30 august 19:00
+                                </p>
+                                <h3 class="text-lg font-semibold text-gray-900 ">
+                                    <a href="#" class="hover:underline">Bruno mars</a>
+                                </h3>
+                            </div>
+
+                            <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
+                                <p
+                                    class="w-40 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
+                                    15 october 15:00
+                                </p>
+                                <h3 class="text-lg font-semibold text-gray-900 ">
+                                    <a href="#" class="hover:underline">The weeknd</a>
+                                </h3>
+                            </div>
+
+                            <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
+                                <p
+                                    class="w-40 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
+                                    30 september 11:00
+                                </p>
+                                <h3 class="text-lg font-semibold text-gray-900 ">
+                                    <a href="#" class="hover:underline">Kendrick lamar</a>
+                                </h3>
+                            </div>
+
+                            <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
+                                <p
+                                    class="w-40 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
+                                    13 march 20:00
+                                </p>
+                                <h3 class="text-lg font-semibold text-gray-900 ">
+                                    <a href="#" class="hover:underline">Paramore</a>
+                                </h3>
+                            </div>
+
+                            <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
+                                <p
+                                    class="w-40 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
+                                    12 november 19:00
+                                </p>
+                                <h3 class="text-lg font-semibold text-gray-900 ">
+                                    <a href="#" class="hover:underline">Lil Vasco</a>
+                                </h3>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="flex items-center justify-center space-x-2 mt-6">
+                        <!-- Previous Button -->
+                        <button
+                            class="px-4 py-2 text-gray-500 bg-gray-100 rounded-md hover:bg-gray-200 focus:outline-none focus:ring focus:ring-gray-300"
+                            disabled>
+                            Previous
+                        </button>
+
+                        <!-- Page Numbers -->
+                        <button
+                            class="px-4 py-2 text-white bg-neutral-800 rounded-md hover:bg-neutral-800 focus:outline-none focus:ring focus:ring-neutral-300">
+                            1
+                        </button>
+                        <button
+                            class="px-4 py-2 text-gray-700 bg-gray-100 rounded-md hover:bg-gray-200 focus:outline-none focus:ring focus:ring-gray-300">
+                            2
+                        </button>
+                        <button
+                            class="px-4 py-2 text-gray-700 bg-gray-100 rounded-md hover:bg-gray-200 focus:outline-none focus:ring focus:ring-gray-300">
+                            3
+                        </button>
+                        <span class="px-4 py-2 text-gray-700">...</span>
+                        <button
+                            class="px-4 py-2 text-gray-700 bg-gray-100 rounded-md hover:bg-gray-200 focus:outline-none focus:ring focus:ring-gray-300">
+                            10
+                        </button>
+
+                        <!-- Next Button -->
+                        <button
+                            class="px-4 py-2 text-gray-500 bg-gray-100 rounded-md hover:bg-gray-200 focus:outline-none focus:ring focus:ring-gray-300">
+                            Next
+                        </button>
                     </div>
 
-                    <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
-                        <p class="w-32 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
-                            10:00 - 11:00
-                        </p>
-                        <h3 class="text-lg font-semibold text-gray-900 dark:">
-                            <a href="#" class="hover:underline">Flowbite - An Open Framework for Forensic
-                                Watermarking</a>
-                        </h3>
-                    </div>
-
-                    <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
-                        <p class="w-32 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
-                            11:00 - 12:00
-                        </p>
-                        <h3 class="text-lg font-semibold text-gray-900 ">
-                            <a href="#" class="hover:underline">Coffee Break</a>
-                        </h3>
-                    </div>
-
-                    <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
-                        <p class="w-32 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
-                            12:00 - 13:00
-                        </p>
-                        <h3 class="text-lg font-semibold text-gray-900 ">
-                            <a href="#" class="hover:underline">Scaling your brand from €0 to multimillion euros</a>
-                        </h3>
-                    </div>
-
-                    <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
-                        <p class="w-32 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
-                            13:00 - 14:00
-                        </p>
-                        <h3 class="text-lg font-semibold text-gray-900 ">
-                            <a href="#" class="hover:underline">Updates from the Open Source Multimedia community</a>
-                        </h3>
-                    </div>
-
-                    <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
-                        <p class="w-32 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
-                            14:00 - 15:00
-                        </p>
-                        <h3 class="text-lg font-semibold text-gray-900 ">
-                            <a href="#" class="hover:underline">Exploring the balance between customer acquisition and
-                                retention</a>
-                        </h3>
-                    </div>
-
-                    <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
-                        <p class="w-32 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
-                            15:00 - 16:00
-                        </p>
-                        <h3 class="text-lg font-semibold text-gray-900 ">
-                            <a href="#" class="hover:underline">Flowbite - An Open Framework for Forensic
-                                Watermarking</a>
-                        </h3>
-                    </div>
-
-                    <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
-                        <p class="w-32 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
-                            16:00 - 14:00
-                        </p>
-                        <h3 class="text-lg font-semibold text-gray-900 ">
-                            <a href="#" class="hover:underline">Scaling your brand from €0 to multimillion euros</a>
-                        </h3>
-                    </div>
-
-                    <div class="flex flex-col gap-2 py-4 sm:gap-6 sm:flex-row sm:items-center">
-                        <p class="w-32 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0">
-                            17:00 - 14:00
-                        </p>
-                        <h3 class="text-lg font-semibold text-gray-900 ">
-                            <a href="#" class="hover:underline">Drinks & networking</a>
-                        </h3>
-                    </div>
                 </div>
+                <img class="w-full rounded-lg " src="{{ Vite::asset('resources/assets/slipknotlive.jpg') }}"
+                    alt="dashboard image">
+
             </div>
         </div>
     </section>
