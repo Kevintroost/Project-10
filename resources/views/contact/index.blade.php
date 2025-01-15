023e1af9244a943a4602dca9bf141b830bcfc788
KEVN <troostkevin8@gmail.com>
2025-01-15 12:09:34 +0100

    contact admin af

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -1,38 +1,36 @@
 <x-admin-layout>
-    <section class="p-6 flex flex-row gap-6 max-w-screen-xl mx-auto px-8 mx-auto max-w-screen-xl">
-        <div
-            class="w-full md:w-auto bg-white border border-gray-200 rounded-lg shadow-md p-6 overflow-y-auto flex-shrink-0 h-[16rem]">
-            <div class="grid grid-cols-1 gap-6">
-                @foreach ($contacts as $contact)
-                    <div class="flex items-center justify-between">
-                        <div class="flex flex-col items-start ml-4">
-                            <p class="text-sm text-gray-700">{{ $contact->name }}</p>
-                        </div>
-                        <div class="flex flex-col items-start ml-4">
-                            <p class="text-sm text-gray-700">{{ $contact->email }}</p>
-                        </div>
-                        <div class="flex flex-col items-start ml-4">
-                            <p class="text-sm text-gray-700">{{ $contact->phonenumber }}</p>
-                        </div>
-                        <div class="flex flex-col items-start ml-4">
-                            <p class="text-sm text-gray-700">{{ $contact->message }}</p>
-                        </div>
+<section class="p-6 flex flex-row gap-6 max-w-screen-xl mx-auto">
+    <div class="flex flex-col items-stretch w-full">
 
-                        <form action="{{ route('contact.destroy', ['id' => $contact->id]) }}" method="POST">
-                            @method('DELETE')
-                            @csrf
-                            <input type="hidden" name="event_id" value="{{ $contact->id }}">
-                            <button type="submit"
-                                class="mt-2 w-full bg-red-600 text-white font-semibold text-sm rounded-lg py-3 px-10 focus:outline-none hover:bg-red-700 cursor-pointer">
-                                Delete
-                            </button>
-                        </form>
-
-                    </div>
-                @endforeach
+        @foreach ($contacts as $contact)
+            <div class="flex flex-row items-center justify-between  border rounded-lg shadow-lg rounded-lg p-4 mb-4 shadow-sm bg-white">
+                <div class="flex-1">
+                    <p class="text-sm text-gray-700 font-medium">{{ $contact->name }}</p>
+                </div>
+                <div class="flex-1">
+                    <p class="text-sm text-gray-700">{{ $contact->email }}</p>
+                </div>
+                <div class="flex-1">
+                    <p class="text-sm text-gray-700">{{ $contact->phonenumber }}</p>
+                </div>
+                <div class="flex-1">
+                    <p class="text-sm text-gray-700">{{ $contact->message }}</p>
+                </div>
+                <div class="flex-none">
+                    <form action="{{ route('contact.destroy', ['id' => $contact->id]) }}" method="POST" class="ml-4">
+                        @method('DELETE')
+                        @csrf
+                        <input type="hidden" name="event_id" value="{{ $contact->id }}">
+                        <button type="submit" class="mt-2 bg-red-600 text-white font-semibold text-sm rounded-lg py-2 px-4 focus:outline-none hover:bg-red-700 cursor-pointer">
+                            Delete
+                        </button>
+                    </form>
+                </div>
             </div>
-        </div>
-    </section>
+        @endforeach
+    </div>
+</section>
+
 
 
 </x-admin-layout>
