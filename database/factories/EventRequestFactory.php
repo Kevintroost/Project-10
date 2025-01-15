2a2beab7f7c9ddc0b340ef18ddbc0f2cf0fcaa6d
Aaron24115 <9018817@student.zadkine.nl>
2025-01-15 12:10:31 +0100

    update

--- database/factories/EventRequestFactory.php
+++ database/factories/EventRequestFactory.php
@@ -3,7 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-<<<<<<< Updated upstream
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\EventRequest>
