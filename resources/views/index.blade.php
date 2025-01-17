a853ac87ecb5de07ebf4d93c76a22a6c584f99e0
KEVN <troostkevin8@gmail.com>
2025-01-17 22:03:33 +0100

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -69,6 +69,68 @@
 
 
     </section>
+    <section class="bg-white">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-8 lg:px-6">
+        <!-- Centered Section Title -->
+        <div class="max-w-screen-md mb-8 lg:mb-16 text-center mx-auto">
+            <h2 class="mb-4 text-4xl font-extrabold text-gray-900 ">Discover more</h2>
+        </div>
+        
+        <!-- Left-aligned Grid for Icons and Content -->
+        <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+            <div class="flex items-start space-x-4">
+                <div class="flex justify-center items-center mb-4 w-12 p-2 rounded-full bg-blue-500">
+                    <svg class="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                    </svg>
+                </div>
+                <div>
+                    <a href="{{ url('event-request/create') }}">
+
+                    <h3  class="mb-2 text-xl font-bold hover:underline ">Event request</h3>
+                    </a>
+                    <p class="text-gray-500 dark:text-gray-400">
+                        Submit your event requests and let us help you bring your vision to life. Our team is dedicated to making your event a success, from planning to execution.
+                </p>
+                </div>
+            </div>
+
+            <div class="flex items-start space-x-4">
+                <div class="flex justify-center items-center mb-4 w-12 p-2 rounded-full bg-blue-500">
+                    <svg class="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"></path>
+                    </svg>
+                </div>
+                <div>
+                    <a href="{{ url('/about-us') }}">
+                    <h3 class="mb-2 text-xl font-bold hover:underline">About us</h3>
+                    </a>
+                    <p class="text-gray-500 dark:text-gray-400">
+                        Learn more about our team, our mission, and our commitment to creating unforgettable experiences. We’re here to help you make your event dreams a reality.        
+                </p>
+                </div>
+            </div>
+
+            <div class="flex items-start space-x-4">
+                <div class="flex justify-center items-center mb-4 w-12 p-2 rounded-full bg-blue-500">
+                    <svg class="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+                        <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path>
+                    </svg>
+                </div>
+                <div>
+                    <a href="{{ url('/contact/create') }}">
+                    <h3 class="mb-2 text-xl font-bold hover:underline ">Contact</h3>
+                    </a>
+                    <p class="text-gray-500 dark:text-gray-400">
+                        Get in touch with us to discuss your event needs, ask questions, or provide feedback. We’re here to help you create the perfect event experience.       
+                </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
 
 
     <section class="bg-white py-8 px-4 mx-auto max-w-screen-xl lg:py-8">
@@ -85,16 +147,7 @@
                     ideas that spark innovation, we’re creating opportunities for people to connect with what
                     they love and with one another.
                 </p>
-                <a href="{{ url('/about-us') }}"
-                    class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 ">
-                    About us
-                    <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                            clip-rule="evenodd"></path>
-                    </svg>
-                </a>
+                
             </div>
             <img class="w-full rounded-lg " src="{{ Vite::asset('resources/assets/events.jpeg') }}"
                 alt="dashboard image">
@@ -113,24 +166,15 @@
                     Our platform brings you closer to the action, offering a seamless way to discover and attend events
                     that match your interests. Join us and be part of unforgettable moments.
                 </p>
-                <a href="{{ url('contact/create') }}"
-                    class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 ">
-                    Contact us
-                    <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                            clip-rule="evenodd"></path>
-                    </svg>
-                </a>
+                
 
             </div>
 
         </div>
-        
-        
-            
-    
+
+
+
+
 
 
         <div class="grid grid-cols-2 md:grid-cols-3 gap-4 mb-16">
@@ -141,7 +185,7 @@
                 </div>
             @endforeach
         </div>
-        
+
 
 
     </section>
