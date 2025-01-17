bd26bb1a0d263e64546d9ca264200de45d3c7acb
KEVN <troostkevin8@gmail.com>
2025-01-17 15:25:10 +0100

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,7 +18,7 @@
                 <div class="flex ">
                     <div class="flex items-center bg-white rounded-lg shadow-lg px-4 py-2">
                         <form action="{{ route('results') }}" method="GET"
-                            class="flex items-center bg-white rounded-lg shadow-lg px-4 py-2">
+                            class="flex items-center bg-white px-4 py-2">
                             <!-- Search Event Input -->
                             <div class="flex items-center border-r border-gray-300 pr-4">
                                 <svg class="w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none"
