647ff6f861f4daa5c0b09114376ce59b802427e7
KEVN <troostkevin8@gmail.com>
2025-01-22 11:17:38 +0100

    Update index.blade.php

--- resources/views/event-request/index.blade.php
+++ resources/views/event-request/index.blade.php
@@ -2,9 +2,9 @@
 
 
     <div id="app">
-        
+        <div class="mr-4">
         <h1 class="text-4xl font-bold border-b border-gray-300 ml-4 mb-6 pt-6 pb-2 ">Event requests </h1>
-        
+        </div>
 
         <task-list></task-list>
     </div>
