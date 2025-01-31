b67dd3f1e8b24234e8665eeac8ccaf59a747858a
PERENSAPP <119853517+PERENSAPP@users.noreply.github.com>
2025-01-31 13:57:00 +0100

    Clean up.

--- app/Models/FotoGalerie.php
+++ app/Models/FotoGalerie.php
@@ -10,10 +10,4 @@ class FotoGalerie extends Model
     use HasFactory;
 
     protected $fillable = ['foto', 'admin_id'];
-
-    public function event()
-    {
-        return $this->belongsTo(Event::class);
-    }
-
 }
